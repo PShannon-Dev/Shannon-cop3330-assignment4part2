1499fd944936a3cc2828bee2ad03ee320a9b6326
paulshannon-coding <mikey4295@hotmail.com>
2021-07-07 22:22:16 -0400

    Updated plan

---
+++ src/test/java/ucf/assignments/ManageItemsControllerTest.java
@@ -0,0 +1,21 @@
+package ucf.assignments;
+
+import org.junit.jupiter.api.Test;
+
+class ManageItemsControllerTest {
+
+    @Test
+    void add_item() {
+        //create new array list
+        //fill text box with
+    }
+
+    @Test
+    void remove_item() {
+        //create array list
+        //add item elements
+        //select items retrieving ids
+        //select remove button
+        //assert id is equal to null
+    }
+}
---
+++ src/test/java/ucf/assignments/ToDoListControllerTest.java
@@ -0,0 +1,33 @@
+package ucf.assignments;
+
+import org.junit.jupiter.api.Test;
+
+class ToDoListControllerTest {
+
+    @Test
+    void add_list() {
+        //manually add toDoList to arrayList
+        //generate title in textfield
+        //add second list to arrayList of identical title
+        //assert that the elements are equal at both indexes
+
+    }
+
+    @Test
+    void remove_multiple_lists() {
+        //create multiple todolists
+        //get the multiple ids in an array
+        //remove lists with the ids
+        //create second list of the same items sin the ones that are to be removed
+        //assert that both the first and second list are equal
+    }
+
+    @Test
+    void remove_single_list(){
+        //create single toDoList
+        //get id of list
+        //remove selected id
+        //create second to do list with same title
+        //assert that both are equal
+    }
+}
